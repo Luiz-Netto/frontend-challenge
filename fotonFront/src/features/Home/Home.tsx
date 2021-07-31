f7ccf01c335bfe94d183d2b52c4f68edfd70f34f
Luiz.venancio <luiznetto@protonmail.com>
2021-07-30 21:20:33 -0300

    Feature: home searchBar visual

--- fotonFront/src/features/Home/Home.tsx
+++ fotonFront/src/features/Home/Home.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, TextInput, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 export const Home = () => {
+  const [search, setSearch] = useState('');
+  console.log(search);
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>HOME SCREEN</Text>
+    <SafeAreaView style={styles.screenContainer}>
+      <View style={styles.searchContainer}>
+        <EvilIcons name="search" size={25} color="#F0F0F0" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search Book"
+          placeholderTextColor="#54565A"
+          style={styles.searchInput}
+        />
+      </View>
+      <Text style={styles.text}>
+        Hi, <Text style={styles.textName}>Mehmed Al Fatih</Text> 👋
+      </Text>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFCF9',
   },
+  searchContainer: {
+    maxHeight: 48,
+    marginHorizontal: 20,
+    marginTop: 50,
+    marginBottom: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  searchInput: {},
   text: {textAlign: 'center', color: '#000000'},
+  textName: {textAlign: 'center', color: '#AA0000'},
 });
--- fotonFront/src/routes/Routes.tsx
+++ fotonFront/src/routes/Routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
 import * as features from '../features';
 
@@ -9,14 +9,32 @@ const BottomTab = createBottomTabNavigator();
 export const Routes = () => {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="Home" component={features.HomeFeature.Home} />
+      <BottomTab.Screen
+        name="Home"
+        component={features.HomeFeature.Home}
+        options={{
+          tabBarIcon: () => {
+            return <Feather name="home" color="#000000" size={18} />;
+          },
+        }}
+      />
       <BottomTab.Screen
         name="Libraries"
         component={features.LibrariesFeature.Libraries}
+        options={{
+          tabBarIcon: () => {
+            return <Feather name="book" color="#000000" size={18} />;
+          },
+        }}
       />
       <BottomTab.Screen
         name="Profile"
         component={features.ProfileFeature.Profile}
+        options={{
+          tabBarIcon: () => {
+            return <Feather name="user" color="#000000" size={18} />;
+          },
+        }}
       />
     </BottomTab.Navigator>
   );
