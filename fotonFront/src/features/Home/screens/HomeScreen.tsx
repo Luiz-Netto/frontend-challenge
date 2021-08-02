77e6be7c33045c66d94b1df11abaa465f79cf680
Luiz.venancio <luiznetto@protonmail.com>
2021-08-01 23:03:21 -0300

    Feature: Home currently reading start

--- fotonFront/src/features/Home/screens/HomeScreen.tsx
+++ fotonFront/src/features/Home/screens/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, ImageBackground, Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import {BookShowcase, SearchBar, SectionTitle} from '../components';
 
@@ -36,6 +37,21 @@ export const HomeScreen = () => {
         />
       </View>
       <SectionTitle title="Currently Reading" link="All" />
+      <View style={styles.readingContainer}>
+        <View>
+          {/* <Image style={styles.readingBookCover}/> */}
+          <View style={styles.readingInfo}>
+            <Text style={styles.readingTitle}>Title</Text>
+            <Text style={styles.readingAuthor}>Author</Text>
+            <View style={styles.readingChapterContainer}>
+              <AntDesign name="book" size={14} color="#9013FE" />
+              <Text style={styles.readingChapterText}>
+                chapter<Text style={styles.readingChapterNumber}>2</Text>from 9
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
       <SectionTitle title="Reviews of The Days" link="All video" />
     </SafeAreaView>
   );
@@ -53,4 +69,29 @@ const styles = StyleSheet.create({
   greetingsText: {textAlign: 'center', color: '#000000', fontSize: 24},
   greetingsTextName: {textAlign: 'center', color: '#F65066', fontSize: 24},
   carouselContent: {marginLeft: 20},
+  readingContainer: {
+    backgroundColor: '#EEF5DB',
+    height: 100,
+    marginRight: 44,
+    marginTop: 30,
+    marginBottom: 45,
+    borderTopRightRadius: 3,
+    borderBottomRightRadius: 3,
+  },
+  readingBookCover: {
+    maxHeight: 140,
+    maxWidth: 100,
+    position: 'relative',
+    bottom: 20,
+    top: 20,
+  },
+  readingInfo: {marginTop: 16, marginBottom: 10, marginLeft: 10},
+  readingTitle: {fontSize: 20, letterSpacing: 2, color: '#2A2B26'},
+  readingAuthor: {fontSize: 10, color: '#74776D'},
+  readingChapterContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  readingChapterText: {fontSize: 10, color: '#2A2B26', letterSpacing: 0.02},
+  readingChapterNumber: {fontSize: 10, color: '#F65066', letterSpacing: 0.02},
 });
